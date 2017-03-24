8b046adac28ff4163821160f62d14d7e22f6cdec
Jonathan Lu <jonathanlu101@gmail.com>
2017-03-24 19:34:24 +1100

    Added timeFromNow Field for nearbyStops

--- TransportApp/Scripts/app/shared/filters.js
+++ TransportApp/Scripts/app/shared/filters.js
@@ -1,10 +1,23 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('TransportApp').filter('dateFilter', function () {
+    angular.module('TransportApp').filter('timeFromNow', function () {
         return function (input) {
             var date = new Date(input);
-            return date.getMonth();
+            var now = new Date();
+
+            var differenceMs = date.getTime() - now.getTime();
+            var differenceMinutes = Math.floor(differenceMs / (1000 * 60)); 
+
+            if (differenceMinutes < 60) {
+                var differenceString = differenceMinutes + "m";
+            } else {
+                var hours = Math.floor(differenceMinutes / 60);
+                var minutes = differenceMinutes % 60;
+                var differenceString = `${hours}h ${minutes}m`;
+            }
+             
+            return differenceString;
         }
     });
 
